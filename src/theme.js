14c1557a4f329834aa118f4bbbbe7554445313f5
Erdem Bozkurt <ebozkurt93@gmail.com>
2020-02-29 15:48:32 +0100

    Changed theming

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { dark } from 'mdx-deck'
+
+export default {
+    ...dark,
+    googleFont: "https://fonts.googleapis.com/css?family=Fira+Mono|Lato&display=swap",
+    fonts: {
+        body: '"Lato", sans-serif',
+        monospace: '"Fira Mono", monospace',
+    },
+    colors: {
+        text: 'rgba(255, 255, 255, 0.9)',
+        background: '#27292d'
+    },
+}
