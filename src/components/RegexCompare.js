f996f3df2cef278134aa2a6017567e02a795043b
Erdem Bozkurt <ebozkurt93@gmail.com>
2020-03-08 16:48:01 +0100

    Added ability to highlight found parts in text

--- src/components/RegexCompare.js
+++ src/components/RegexCompare.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-export default function RegexCompare({ text, pattern, answer }) {
+export default function RegexCompare({ text, pattern }) {
   const inputFocusNotAllowedKeys = new Set([
     'ArrowLeft',
     'ArrowRight',
@@ -12,19 +12,64 @@ export default function RegexCompare({ text, pattern, answer }) {
   const inputFocusDisableKeys = new Set(['Escape', 'Tab']);
 
   const [currentPattern, setCurrentPattern] = useState(pattern);
+  const [highlightedText, setHighlightedText] = useState(text);
   const isPatternMatchingText = (text, pattern) => {
     try {
       new RegExp(pattern, 'g');
     } catch (error) {
       return false;
     }
-    const re = new RegExp(pattern, 'g');
-    const matches = text.match(re);
-    return matches && matches.find(r => r === text);
+    var re = new RegExp(pattern, 'g');
+
+    var match;
+    var highlightedTextContent = [];
+    var prevStartIndex = 0;
+    var prevEndIndex = 0;
+    while (pattern.length > 0 && pattern !== '()' && (match = re.exec(text))) {
+      highlightedTextContent.push(text.substring(prevEndIndex, match.index));
+      prevStartIndex = match.index;
+      prevEndIndex = match.index + match[0].length;
+      highlightedTextContent.push(
+        <span
+          key={match.index}
+          style={{ background: '#18B70F', borderRadius: '3px' }}
+        >
+          {text.substring(prevStartIndex, prevEndIndex)}
+        </span>
+      );
+      var res = false;
+      if (match[0] === text) {
+        highlightedTextContent = [
+          <span
+            key={match.index}
+            style={{ background: '#18B70F', borderRadius: '3px' }}
+          >
+            {text}
+          </span>
+        ];
+        res = true;
+        break;
+      }
+    }
+    if (highlightedTextContent.length === 0) {
+      highlightedTextContent.push(<span key={0}>{text}</span>);
+    } else {
+      highlightedTextContent.push(
+        <span key={text.length}>
+          {text.substring(prevEndIndex, text.length)}
+        </span>
+      );
+    }
+    setHighlightedText(highlightedTextContent);
+    return res;
   };
-  const [correctAnswer, setCorrectAnswer] = useState(
-    isPatternMatchingText(text, pattern)
-  );
+
+  const [correctAnswer, setCorrectAnswer] = useState(false);
+
+  useEffect(() => {
+    setCorrectAnswer(isPatternMatchingText(text, pattern));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleKeyDown = e => {
     if (inputFocusDisableKeys.has(e.key)) {
@@ -34,13 +79,17 @@ export default function RegexCompare({ text, pattern, answer }) {
       e.nativeEvent.stopImmediatePropagation();
     }
   };
+
   const handleChange = e => {
     setCurrentPattern(e.target.value);
     setCorrectAnswer(isPatternMatchingText(text, e.target.value));
   };
+
   return (
     <div>
-      <p>Search Text: {text}</p>
+      <p>
+        Searching for: <b>{highlightedText}</b>
+      </p>
       <Input
         type="text"
         name="pattern"
@@ -56,8 +105,7 @@ export default function RegexCompare({ text, pattern, answer }) {
 
 RegexCompare.propTypes = {
   text: PropTypes.string.isRequired,
-  pattern: PropTypes.string,
-  answer: PropTypes.string
+  pattern: PropTypes.string
 };
 
 const Input = styled.input`
